8c68d0a74cc99ddd414cbb24e55a8df0cab3905a
Josiel <josielsifa@gmail.com>
2021-01-07 14:51:07 -0300

    Adicionando configuração do routlet e pagina de 404

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
 import { ContainerComponent } from './components/container/container.component';
 import { InfoHomeComponent } from './services/info-home/info-home.component';
+import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     ContainerComponent,
-    InfoHomeComponent
+    InfoHomeComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
